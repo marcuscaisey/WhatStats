dbc0a45061be2200aa1ad64396279c371f50912c
Marcus Caisey <34950778+marcuscaisey@users.noreply.github.com>
2018-03-24 05:44:10 +0000

    Move chat loading to seperate thread

--- core/main.py
+++ core/main.py
@@ -1,3 +1,4 @@
+import threading
 from pathlib import Path
 from zipfile import ZipFile
 
@@ -12,11 +13,51 @@ TEMP_PATH = Path('temp')
 CHAT_LOG_NAME = '_chat.txt'
 CHAT_LOG_PATH = TEMP_PATH / CHAT_LOG_NAME
 
+CHAT_LOAD_EVENT_TYPE = wx.NewEventType()
+CHAT_LOAD_EVENT_BINDER = wx.PyEventBinder(CHAT_LOAD_EVENT_TYPE)
 
-def extract_chat_log(zip_path, destination_path):
-    """Extract chat log from chat log zip to destination path."""
-    with ZipFile(zip_path) as zip_obj:
-        zip_obj.extract(CHAT_LOG_NAME, path=str(destination_path))
+
+class ChatLoadThread(threading.Thread):
+    """
+    Thread which: extracts chat log from zip file, initialises chat
+    object from chat log, then posts ChatLoadEvent back to main thread.
+    """
+
+    def __init__(self, parent, zip_path):
+        super().__init__()
+        self.parent = parent
+        self.zip_path = zip_path
+
+    def extract_chat_log(zip_path, destination_path):
+        """Extract chat log from chat log zip to destination path."""
+        with ZipFile(zip_path) as zip_obj:
+            zip_obj.extract(CHAT_LOG_NAME, path=str(destination_path))
+
+    def run(self):
+        try:
+            self.extract_chat_log(self.zip_path, TEMP_PATH)
+            with LoadingDialog(self.parent) as loading_dialog:
+                chat = Chat(CHAT_LOG_PATH, loading_dialog)
+                CHAT_LOG_PATH.unlink()
+                import_event = ChatLoadEvent(chat)
+                wx.PostEvent(self.parent, import_event)
+        except OSError:
+            wx.LogError('Couldn\'t open zip file.')
+        except KeyError:
+            wx.LogError('Couldn\'t extract chat log.')
+        except ValueError:
+            wx.LogError('Chat log not valid.')
+
+
+class ChatLoadEvent(wx.PyCommandEvent):
+    """
+    Event that signals chat object has been initialised and is ready to
+    be loaded.
+    """
+
+    def __init__(self, chat):
+        super().__init__(eventType=CHAT_LOAD_EVENT_TYPE)
+        self.chat = chat
 
 
 class WhatStats(wx.App):
@@ -26,6 +67,7 @@ class WhatStats(wx.App):
         super().__init__()
         self.frame = MainFrame()
         self.panel = self.frame.panel
+        self.chat = None
         self.bind_event_handlers()
 
     def start(self):
@@ -38,11 +80,11 @@ class WhatStats(wx.App):
         """Bind events to their event handlers."""
         self.frame.Bind(wx.EVT_CLOSE, self.on_close)
 
+        self.frame.Bind(CHAT_LOAD_EVENT_BINDER, self.on_chat_load)
         self.frame.menu_bar.Bind(wx.EVT_MENU, self.on_import, id=wx.ID_OPEN)
         self.frame.menu_bar.Bind(wx.EVT_MENU, self.on_quit, id=wx.ID_EXIT)
 
-        kill_event = wx.EVT_KILL_FOCUS
-        self.panel.subject_input.Bind(kill_event, self.on_subject_change)
+        self.panel.subject_input.Bind(wx.EVT_KILL_FOCUS, self.on_subject_input)
         self.panel.generate_button.Bind(wx.EVT_BUTTON, self.on_generate)
 
     def on_close(self, event):
@@ -56,30 +98,23 @@ class WhatStats(wx.App):
     def on_import(self, event):
         """
         Ask user to select chat log zip, then extract it and initialise
-        Chat object.
+        Chat object in a seperate thread.
         """
-        import_dialog = ImportDialog(self.frame)
-        if import_dialog.ShowModal() != wx.ID_CANCEL:
-            zip_path = import_dialog.GetPath()
-            try:
-                extract_chat_log(zip_path, TEMP_PATH)
-                loading_dialog = LoadingDialog(self.frame)
-                self.chat = Chat(CHAT_LOG_PATH, loading_dialog)
-                loading_dialog.Destroy()
-                CHAT_LOG_PATH.unlink()
-                self.init_inputs(self.chat)
-            except OSError:
-                wx.LogError('Couldn\'t open zip file.')
-            except KeyError:
-                wx.LogError('Couldn\'t extract chat log.')
-            except ValueError:
-                wx.LogError('Chat log not valid.')
-        import_dialog.Destroy()
+        with ImportDialog(self.frame) as import_dialog:
+            if import_dialog.ShowModal() != wx.ID_CANCEL:
+                zip_path = import_dialog.GetPath()
+                thread = ChatLoadThread(self.frame, zip_path)
+                thread.start()
+
+    def on_chat_load(self, event):
+        """Set chat variable and initialise inputs with chat data."""
+        self.chat = event.chat
+        self.init_inputs(self.chat)
 
     def on_quit(self, event):
         self.frame.Close()
 
-    def on_subject_change(self, event):
+    def on_subject_input(self, event):
         """Set chat subject to contents of chat subject input."""
         self.chat.subject = self.panel.subject_input.GetValue()
 
